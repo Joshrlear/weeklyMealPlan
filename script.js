2aba543cd98ff3e440c0bad3c7eb02d69fd5db08
Josh <joshrlear@gmail.com>
2019-04-24 18:51:59 -0700

    update js. add functions that generate html from api

--- script.js
+++ script.js
@@ -18,56 +18,123 @@ const edamamNutritionUrl = 'https://api.edamam.com/api/nutrition-details';
 const edamamFoodUrl = 'https://api.edamam.com/api/food-database/parser';
 
 
-// Page HTML Database
-const HTML = {
-    dietOptions: [
-        
-    ],
-    weekMealPlan: 
-    `<ul id="js-container" class="container">
-    <li id="js-box" class="box box1" data-expand="false">
-      <section class="header-container">
-      <header role="header" id="js-recipe-header" class="recipe-header">
-          <h1>{day}</h1>
-          <h2>{recipe}</h2>
-          <h3>{mealCals}kcal</h3>
-          <p>{ingredient-list-snippet}</p>
-      </header>
-      <img role="img" src="https://www.rd.com/wp-content/uploads/2018/06/healthy-food.jpg" alt="Recipe image for Monday" id="js-recipe-img" class="recipe-img">
-      </section>
-      <section id="js-section" class="section">
-        <section class="recipe-info">
-          <header class="info-header">
-            <h1>{day}</h1>
-            <h2>{recipe}</h2>
-            <h3>{mealCals}kcal per serving | {yields} Servings</h3>
-            <p><em>Takes approximately: {time}</em></p>
-          </header>
-          <article>
-            <h2>Ingredients</h2>
-            <ul id="js-ingredient-list" class="ingredient-list">
-            </ul>
-          </article>
-        </section>
-          </section>
-    </li>
-    </ul>`
-};
+function getWeekDay(i) {
+        const weekArray = {
+            1: 'Monday',
+            2: 'Tuesday',
+            3: 'Wednesday',
+            4: 'Thursday',
+            5: 'Friday',
+            6: 'Saturday',
+            7: 'Sunday',
+            8: 'Monday',
+            9: 'Tuesday',
+            10: 'Wednesday',
+            11: 'Thursday',
+            12: 'Friday',
+            13: 'Saturday'
+        };
 
-function generateIngredientList(Responsejson) {
-    const ingredientList = [];
-    const ingredients = Responsejson.hits[0].recipe.ingredientLines;
-    for (let i of ingredients) {
-        ingredientList.push(`<li>${i}</li>`);
+        const date = new Date();
+        const day = date.getDay() + i;
+        return weekArray[day];
+}
+
+function getRecipeName(Responsejson, i) {
+    return Responsejson.hits[i].recipe.label;
+}
+
+function getCals(Responsejson, i) {
+    const totalCals = Responsejson.hits[i].recipe.calories;
+    const servings = Responsejson.hits[i].recipe.yield;
+    return Math.round(totalCals / servings);
+}
+
+function getServings(Responsejson, i) {
+    return Responsejson.hits[i].recipe.yield;
+}
+
+function getTimeToMake(Responsejson, i) {
+    const time = Responsejson.hits[i].recipe.totalTime;
+    return `${time}mins`
+}
+
+function getSnippet(Responsejson, i) {
+    const ingredients = Responsejson.hits[i].recipe.ingredientLines;
+    const snippet = ingredients.join(', ').substring(0, 22);
+    return snippet;
+}
+
+function getIngredients(Responsejson, i) {
+    const ingredientList = []
+    const ingredients = Responsejson.hits[i].recipe.ingredientLines;
+    for (let x of ingredients) {
+        ingredientList.push(`<li>${x}</li>`);
     }
-    $('#js-main').html(HTML.weekMealPlan);
-    $('#js-ingredient-list').html(ingredientList);
+    return ingredientList;
+}
+
+function getImage(Responsejson, i) {
+    return Responsejson.hits[i].recipe.image;
 }
 
 function displayResults(Responsejson) {
+    $('#js-main').empty();
+    const num = Responsejson.to;
     console.log(Responsejson);
-    generateIngredientList(Responsejson);
+
+    for (let i = 0; i <= num; i++) {
+        const day = getWeekDay(i);
+        console.log(day);
+        const recipe = getRecipeName(Responsejson, i);
+        console.log(recipe);
+        const snippet = getSnippet(Responsejson, i);
+        console.log(snippet);
+        const calories = getCals(Responsejson, i);
+        console.log(calories);
+        const servings = getServings(Responsejson, i);
+        console.log(servings);
+        const timeToMake = getTimeToMake(Responsejson, i);
+        console.log(timeToMake);
+        const ingredients = getIngredients(Responsejson, i);
+        console.log('ingredients');
+        const image = getImage(Responsejson, i);
+        console.log(image);
+        $('#js-main').append(
+            `<ul id="js-container" class="container">
+                <li id="js-box" class="box box1" data-expand="false">
+                  <section class="header-container">
+                  <header role="header" id="js-recipe-header" class="recipe-header">
+                      <h1>${day}</h1>
+                      <h2>${recipe}</h2>
+                      <h3>${calories}kcal</h3>
+                      <p>${snippet}...</p>
+                  </header>
+                  <img role="img" src=${image} alt="Recipe image for Monday" id="js-recipe-img" class="recipe-img">
+                  </section>
+                  <section id="js-section" class="section">
+                    <section class="recipe-info">
+                      <header class="info-header">
+                        <h1>${day}</h1>
+                        <h2>${recipe}</h2>
+                        <h3>${calories}kcal per serving | ${servings} Servings</h3>
+                        <p><em>Takes approximately: ${timeToMake}</em></p>
+                      </header>
+                      <article>
+                        <h2>Ingredients</h2>
+                        <ul id="js-ingredient-list" class="ingredient-list">
+                        ${ingredients}
+                        </ul>
+                      </article>
+                    </section>
+                      </section>
+                </li>
+            </ul>
+            `
+        )
+    }
 }
+
 
 function formatQueryParams(params) {
     /*for (let value in params) {
@@ -117,7 +184,7 @@ function getRecipes(q, diet, calLimit, exclusions) {
         throw new Error(Response.statusText);
     })
     .then(Responsejson => displayResults(Responsejson))
-    .catch(err => {
+    .catch(err => { console.log(err);
         $('#js-main').text(`Uh oh, something went wrong: ${err.message}`);
     });
 }

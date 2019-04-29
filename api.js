0ae438a222ff110681f610ca7e69a07e19089af9
Josh <joshrlear@gmail.com>
2019-04-28 20:57:21 -0700

    updated desktop view css. added pixabay api for background img. connected meal type and updated diet-health label for edamam api

--- api.js
+++ api.js
@@ -7,8 +7,55 @@ const edamamAppId = '97ad01ba';
 // Edamam Recipe API
 const edamamRecipeUrl = 'https://api.edamam.com/search';
 
+// Pixabay Key
+const pixabayKey = "12181901-0ae683bad462aef9e23b07d08";
+
+// Pixabay API
+const pixabayurl = "https://pixabay.com/api/";
 
 
+// Values to check query against
+const STORE = {
+    diet: [
+        "balanced",
+        "high-fiber",
+        "high-protein",
+        "low-carb",
+        "low-sodium"
+    ],
+    health: [
+        "alcohol-free",
+        "celery-free",
+        "crustacean-free",
+        "dairy-free",
+        "egg-free",
+        "fish-free",
+        "gluten-free",
+        "keto-friendly",
+        "kidney-friendly",
+        "kosher",
+        "low-potassium",
+        "lupine-free",
+        "mustard-free",
+        "No-oil-added",
+        "low-sugar",
+        "paleo",
+        "peanut-free",
+        "pescatarian",
+        "pork-free",
+        "red-meat-free",
+        "sesame-free",
+        "shellfish-free",
+        "soy-free",
+        "sugar-conscious",
+        "tree-nut-free",
+        "vegan",
+        "vegetarian",
+        "wheat-free"
+    ]
+}
+
+// Determine today and get remaining 6 days
 function getWeekDay(i) {
         const weekArray = {
             1: 'Monday',
@@ -31,31 +78,37 @@ function getWeekDay(i) {
         return weekArray[day];
 }
 
+// Return name of recipe
 function getRecipeName(Responsejson, i) {
     return Responsejson.hits[i].recipe.label;
 }
 
+// Return calories per serving
 function getCals(Responsejson, i) {
     const totalCals = Responsejson.hits[i].recipe.calories;
     const servings = Responsejson.hits[i].recipe.yield;
     return Math.round(totalCals / servings);
 }
 
+// Return amount of servings
 function getServings(Responsejson, i) {
     return Responsejson.hits[i].recipe.yield;
 }
 
+// Return time to cook per recipe
 function getTimeToMake(Responsejson, i) {
     const time = Responsejson.hits[i].recipe.totalTime;
     return `<b>${time}mins</b>`
 }
 
+// Create a small snippet of the top ingredients
 function getSnippet(Responsejson, i) {
     const ingredients = Responsejson.hits[i].recipe.ingredientLines;
     const snippet = ingredients.join(', ').substring(0, 22);
     return snippet;
 }
 
+// Return list of ingredients per recipe
 function getIngredients(Responsejson, i) {
     const ingredientList = []
     const ingredients = Responsejson.hits[i].recipe.ingredientLines;
@@ -66,17 +119,21 @@ function getIngredients(Responsejson, i) {
     return ingredientString;
 }
 
+// Return recipe image
 function getImage(Responsejson, i) {
     return Responsejson.hits[i].recipe.image;
 }
 
+
+// Clears previous values and displays new week meal plan results
 function displayResults(Responsejson) {
     $('#js-container').empty();
     
-    //$('#js-main').append(`<ul id="js-container" class="container"></ul>`)
     const num = Responsejson.to;
     console.log(Responsejson);
 
+    
+    // Generate 7 day meal plan
     for (let i = 0; i < num; i++) {
         const day = getWeekDay(i);
         const recipe = getRecipeName(Responsejson, i);
@@ -109,9 +166,9 @@ function displayResults(Responsejson) {
                     <p${calories}kcal per serving | ${servings} Servings</p>
                     <p><em>Takes approximately: ${timeToMake}</em></p>
                   </header>
-                  <article>
+                  <article role="article">
                     <h2>Ingredients</h2>
-                    <ul id="js-ingredient-list" class="ingredient-list">
+                    <ul role="list" id="js-ingredient-list" class="ingredient-list">
                     ${ingredients}
                     </ul>
                   </article>
@@ -126,23 +183,23 @@ function displayResults(Responsejson) {
 }
 
 
+// Formats parameters for edamam api
 function formatQueryParams(params) {
-    /*for (let value in params) {
-        if (params[value] === 'N/A' || params[value] === undefined || params[value] === null || params[value] === '') {
-            delete params[value];
-        }
-    }*/
     console.log(params);
     const queryItems = Object.keys(params).map(key =>
         `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
         return queryItems.join('&');
 }
 
-function getRecipes(q, diet, calLimit, exclusions) {
+
+// Compile query and call edamam api
+function getRecipes(q, diet, health, dishType, calLimit, exclusions) {
 
     const query = {
         q: q,
-        //diet: diet,
+        diet: diet,
+        health: health,
+        dishtype: dishType,
         calories: calLimit,
         excluded: exclusions,
         to: 7,
@@ -150,6 +207,7 @@ function getRecipes(q, diet, calLimit, exclusions) {
         app_key: edamamKey
     };
 
+    // Filters blank or invalid query params
     const params = generateParam(query);
     function generateParam(query) {
         let newKey = {};
@@ -161,11 +219,12 @@ function getRecipes(q, diet, calLimit, exclusions) {
         return newKey;
     };
 
-    
-
+    // Generate args for edamam api
     const queryString = formatQueryParams(params);
     const url = edamamRecipeUrl + '?' + queryString;
+    
 
+    // Edamam api
     fetch(url)
     .then(Response => {
         if(Response.ok) {
@@ -174,11 +233,12 @@ function getRecipes(q, diet, calLimit, exclusions) {
         throw new Error(Response.statusText);
     })
     .then(Responsejson => displayResults(Responsejson))
-    .catch(err => { console.log(err);
+    .catch(err => {
         $('#js-main').html(`<div class="err-message"><h3>Uh oh, something went wrong: ${err.message}</h3><br>
         <p>Your parameters may be too retricitve. Try changing your search parameters and try again.</p></div>`);
     });
 }
+
 
 // Seperate any exclusions by commas. Removes the following: " ", ",", "and" 
 function generateEx(exclude) {
@@ -186,43 +246,127 @@ function generateEx(exclude) {
 }
 
 
-// Runs after clicking search button on start page
-function handleSearch() {
-    $('#js-form').submit(event => {
-        event.preventDefault();
-        const query = $('#js-search-query').val();
-        const diet = $('#js-diet-dropdown').val();
-        const calories = $('#js-input').val();
-        const exclude = $('#js-textarea').val();
+// Determines whether diet is a query parameter
+function isDiet(dietType) {
+    for (let i of STORE.diet) {
+        console.log(i);
+        if (i === dietType) {
+            return dietType;
+        }
+        else {
+            return null;
+        }
+    }
+};
 
-        // in the future make diet/health it's own param for better results
-        const q = query ? `${query}, ${diet}` : `healthy, ${diet}`;
-        const calLimit = calories ? `0-${calories}` : calories;
-        const exclusions = generateEx(exclude);
-        getRecipes(q, diet, calLimit, exclusions);
-        $('#js-main').toggleClass('form-hidden');
-        $('#js-main').toggleClass('container-hidden');
-        $('#js-container').toggleClass('hidden');
-        $('#js-form').toggleClass('hidden');
-        $('#js-menu-toggle').one().fadeIn(750);
-        $('#js-menu-input').prop('checked', false);
-    })
+// Determines whether health is a query parameter
+function isHealth(dietType) {
+    for (let i of STORE.health) {
+        console.log(i);
+        if (i === dietType) {
+            return dietType;
+        }
+        else {
+            return null;
+        }
+    }
+};
+
+
+// Handles all css related tasks after clicking Search button
+function handleCssOnSearch() {
+    $('#js-background').fadeToggle(750);
+    $('#js-main').toggleClass('form-hidden');
+    $('#js-main').toggleClass('container-hidden');
+    $('#js-container').toggleClass('hidden');
+    $('#js-form').toggleClass('hidden');
+    $('#js-menu-toggle').one().fadeIn(750);
+    $('#js-menu-input').prop('checked', false);
 }
 
 
-function handleMenuClick() {
+// Handles css functions when clicking hamburger menu button
+function handleCssOnMenu() {
     $('#js-menu-input').on('click', function() {
+        $('#js-background').fadeToggle(750);
         $('#js-container').toggleClass('hidden');
-        $('#js-form').toggleClass('hidden');
+        $('#js-form').toggleClass('hidden')
         $('#js-main').toggleClass('form-hidden')
         $('#js-main').toggleClass('container-hidden');
     })
 }
 
+// Runs after clicking search button on start page
+function handleSearch() {
+    $('#js-form').submit(event => {
+        event.preventDefault();
+        const query = $('#js-search-query').val();
+        const dishType = $('#js-meal-dropdown').val();
+        const dietType = $('#js-diet-dropdown').val();
+        const calories = $('#js-input').val();
+        const exclude = $('#js-textarea').val();
+
+        // in the future make diet/health it's own param for better results
+        const q = query ? query : `food`;
+        const diet = isDiet(dietType);
+        const health = isHealth(dietType);
+        const calLimit = calories ? `0-${calories}` : calories;
+        const exclusions = generateEx(exclude);
+        getRecipes(q, diet, health, dishType, calLimit, exclusions);
+        handleCssOnSearch()
+    })
+}
+
+function displayBackground(Responsejson) {
+    const i = Math.round(Math.random());
+    const img = '<img class="background-img-api"src="' + Responsejson.hits[i].largeImageURL + '"></img>';
+    $('#js-background').html(img);
+}
+
+// Generate random image for background
+function backgroundImage() {
+    const foodList = [
+        'breakfast',
+        'omelette',
+        'pancakes',
+        'snack',
+        'dessert',
+        'steak',
+        'chicken',
+        'dinner',
+        'pizza',
+        'fruit',
+        'chocolate',
+        'strawberry',
+        'salad',
+        'baked',
+        'bbq+fried',
+        'hamburger',
+        'waffles',
+        'eggs'
+    ]
+
+    const i = Math.round(Math.random() * 17);
+    const foodType = `q=${foodList[i]}`;
+
+    const url = pixabayurl + '?' + 'key=' + pixabayKey + '&image_type=photo' + '&'+ foodType + '&editors_choice=true';
+    console.log(url);
+    fetch(url)
+    .then(Response => {
+        if(Response.ok) {
+            return Response.json();
+        }
+        throw new Error(Response.statusText);
+    })
+    .then(Responsejson => displayBackground(Responsejson))
+    .catch(err => {
+        console.log(err);
+    });
+}
+
 // Runs on page load
 $(function onLoad() {
-    console.log('load');
-    $('#js-menu-toggle').hide();
+    backgroundImage()
     handleSearch();
-    handleMenuClick();
+    handleCssOnMenu();
 })
